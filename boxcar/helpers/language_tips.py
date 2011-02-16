37cd0d1c0ba1383866e116670086b4f4d026cad1
Tualatrix Chou <tualatrix@gmail.com>
2011-02-16 19:53:09 +0800

    Use the real page;

--- boxcar/helpers/language_tips.py
+++ boxcar/helpers/language_tips.py
@@ -2,11 +2,12 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+import urllib
 
 from BeautifulSoup import BeautifulSoup, Tag, NavigableString
 
 def get_title_and_link():
-    soup = BeautifulSoup(open('/Users/tualatrix/.virtualenvs/ttime/src/boxcar/boxcar/helpers/trans_collect.html').read())
+    soup = BeautifulSoup(urllib.urlopen('http://www.chinadaily.com.cn/language_tips/trans/trans_collect.html').read())
     tips = soup.findAll('a', attrs={'target': '_blank'})
 
     for tip in reversed(tips):
@@ -20,6 +21,6 @@ def get_title_and_link():
         yield title, link
 
 def get_content_from_url(url):
-    soup = BeautifulSoup(open('/Users/tualatrix/.virtualenvs/ttime/src/boxcar/boxcar/helpers/content_12022973.html').read())
+    soup = BeautifulSoup(urllib.urlopen(url).read())
     div = soup.findAll('div', attrs={'class': 'sanjineiwen'})[0]
     return div.renderContents()
