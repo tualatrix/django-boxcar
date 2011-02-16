bdb45b1b20986bcb1f095de8f87e24ef0c2c688a
Tualatrix Chou <tualatrix@gmail.com>
2011-02-16 19:50:50 +0800

    Only deal with the item who is NavigableString

--- boxcar/helpers/language_tips.py
+++ boxcar/helpers/language_tips.py
@@ -3,13 +3,15 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag, NavigableString
 
 def get_title_and_link():
-    soup = BeautifulSoup(open('/home/ted/.virtualenvs/ttime/src/boxcar/boxcar/helpers/trans_collect.html').read())
+    soup = BeautifulSoup(open('/Users/tualatrix/.virtualenvs/ttime/src/boxcar/boxcar/helpers/trans_collect.html').read())
     tips = soup.findAll('a', attrs={'target': '_blank'})
 
     for tip in reversed(tips):
+        if type(tip.contents[0]) != NavigableString:
+            continue
         title = tip.contents[0]
         for k, v in tip.attrs:
             if k == 'href':
@@ -18,6 +20,6 @@ def get_title_and_link():
         yield title, link
 
 def get_content_from_url(url):
-    soup = BeautifulSoup(open('/home/ted/.virtualenvs/ttime/src/boxcar/boxcar/helpers/content_12022973.html').read())
+    soup = BeautifulSoup(open('/Users/tualatrix/.virtualenvs/ttime/src/boxcar/boxcar/helpers/content_12022973.html').read())
     div = soup.findAll('div', attrs={'class': 'sanjineiwen'})[0]
     return div.renderContents()
